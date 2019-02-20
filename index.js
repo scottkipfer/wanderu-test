d2f8f758ad5483ad869945051033a91cf978fe46
Scott Kipfer <scottkipfer@gmail.com>
2019-02-19 22:17:25 -0500

    clean up

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { getStationsNear } = require('./lib/db.api');
+const { getStationsNear } = require('./lib/DBApi');
 const { randomPick, calcDistance } = require('./lib/utils');
-const { createPairs, printPairs, byPublicTransport, byParking } = require('./lib/station');
+const { createStationPairs, printPairs, byPublicTransport, byParking } = require('./lib/station');
 
 const Berlin = {lat: '52.5200' , lng: '13.4050'};
 const Hamburg = {lat: '53.5511' , lng: '9.9937'};
@@ -13,14 +13,13 @@ async function main() {
     berlinStations = berlinStations.filter(byPublicTransport);
     hamburgStations = hamburgStations.filter(byParking);
 
-    const pairs = createPairs(hamburgStations, berlinStations);
+    const pairs = createStationPairs(hamburgStations, berlinStations);
     const chosen = randomPick(pairs, 5);
     printPairs(chosen);
-    
+
   } catch(e) {
     console.log(e);
   }
 }
 main();
-
 
