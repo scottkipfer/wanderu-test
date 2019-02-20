b5716ccd80bf65071af2ff926b3cfdda0bfd3bec
Scott Kipfer <scottkipfer@gmail.com>
2019-02-19 21:50:19 -0500

    made api call clearer

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { getNearBy } = require('./lib/db.api');
+const { getStationsNear } = require('./lib/db.api');
 const { randomPick, calcDistance } = require('./lib/utils');
 const { createPairs, printPairs, byPublicTransport, byParking } = require('./lib/station');
 
@@ -7,7 +7,7 @@ const Hamburg = {lat: '53.5511' , lng: '9.9937'};
 
 async function main() {
   try {
-    const queries = [getNearBy(Berlin), getNearBy(Hamburg)];
+    const queries = [getStationsNear(Berlin), getStationsNear(Hamburg)];
     let [berlinStations, hamburgStations] = await Promise.all(queries);
 
     berlinStations = berlinStations.filter(byPublicTransport);
