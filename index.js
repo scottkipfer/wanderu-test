6250b5fedcf44108e1b067f7f3d612bd7dc3407a
Scott Kipfer <scottkipfer@gmail.com>
2019-02-19 21:47:58 -0500

    removed logs

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const Hamburg = {lat: '53.5511' , lng: '9.9937'};
 async function main() {
   try {
     const queries = [getNearBy(Berlin), getNearBy(Hamburg)];
-    console.log(Date.now())
     let [berlinStations, hamburgStations] = await Promise.all(queries);
-    console.log(Date.now())
 
     berlinStations = berlinStations.filter(byPublicTransport);
     hamburgStations = hamburgStations.filter(byParking);
@@ -18,7 +16,6 @@ async function main() {
     const pairs = createPairs(hamburgStations, berlinStations);
     const chosen = randomPick(pairs, 5);
     printPairs(chosen);
-    console.log(Date.now())
     
   } catch(e) {
     console.log(e);
